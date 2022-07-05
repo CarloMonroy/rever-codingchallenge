f90ddfbd4cb712367197ada7cc33253d6b6286aa
CarloMonroy <carlomonroy1997@gmail.com>
2022-07-05 16:21:22 -0500

    create test for challenge3

--- test_challenge3.py
+++ test_challenge3.py
@@ -1,8 +1,33 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+import numpy as np
+from pandas.testing import assert_frame_equal
+import pandas as pd
+
+from challenge3 import process_file
+import numpy as np
+
+
+class TestChallenge3(unittest.TestCase):
+    def test_process_file(self):
+        test_data1 = {'tax_file_no': ["", "", ""],
+                      'name': ["Makima", "Reeze", "Juam"],
+                      'is_married': ['Y', 'N', 5],
+                      'annual_salary': [20, 30, 50],
+                      'employment_status': ['PE', 'PE', 'CA']
+                      }
+        cleaned_data = {'name': ["Makima", "Reeze", "Juam"],
+                        'is_married': ["Y", "N", np.nan],
+                        'annual_salary': [20, 30, 50],
+                        'employment_status': ['PE', 'PE', 'CA']
+                        }
+
+        test_df = pd.DataFrame.from_dict(test_data1)
+        cleaned_df = pd.DataFrame.from_dict(cleaned_data)
+        processed_df = process_file(test_df)
+
+        assert_frame_equal(cleaned_df, processed_df)
+
 
 if __name__ == '__main__':
     unittest.main()
